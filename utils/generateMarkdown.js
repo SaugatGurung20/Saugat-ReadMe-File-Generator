407416278dfb177bd33af972450f507fee7e9a91
Saugat Gurung <grgseol2015@gmail.com>
2024-02-07 18:02:33 +0000

    completed  function generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,33 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Questions
+For any questions regarding this project, please contact ${data.email}. You can also visit my [GitHub profile](https://github.com/${data.github}) for additional information.
+
+${data.screenshot ? `![Screenshot](${data.screenshot})` : ''}
+`;
+}
+
+module.exports = generateMarkdown;
